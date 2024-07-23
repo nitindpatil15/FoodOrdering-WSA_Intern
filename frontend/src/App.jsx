0dde41d345fdc4423db7df455c23749443d35f64
nitindpatil15 <nitindpatil2002@gmail.com>
2024-07-23 16:22:11 +0530

    WSA-internProject-Food Ordering

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/layouts/Header";
+
+export default function App() {
+  return <div className="App">
+    <Header/>
+  </div>;
+}
---
+++ frontend/src/components/layouts/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Search from "./Search";
+
+const Header = () => {
+  return (
+    <nav className="navbar row sticky-top">
+      <div className="col-12 col-md-3">
+        <img src="/images/logo.webp" alt="logo" className="logo" />
+      </div>
+      <div className="col-12 col-md-6 mt-2 mt-md-6">
+      <Search />
+      </div>
+      <div className="col-12 col-md-3 mt-4 mt-md-0">
+        <span className="ml-3" id="cart">cart</span>
+        <span className="ml-1" id="cart_count">0</span>
+
+        {10<15?(
+            <>
+            <div className="ml-4 dropdown d-inline">
+                <figure className="avatar avatar-nav"><img src="/images/images.png" alt="avatar" className="rounded-circle " /></figure>
+                <span style={{color:"white",fontWeight:"bolder"}}>WSA Dev</span>
+            </div>
+            </>
+        ):(
+            <>
+            <div className="btn ml-4" id="login_btn">Login</div></>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
